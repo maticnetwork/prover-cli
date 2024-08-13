3c52352549ec8f934bd15bc4415dc4b383bcb098
dan moore <dmoore@polygon.technology>
2024-08-13 12:58:22 +0200

    typo

--- prover_cli/report_generator.py
+++ prover_cli/report_generator.py
@@ -91,7 +91,7 @@ def generate_report(witness_dir, metrics_csv_path):
     final_report_df = aggregate_metrics(df, witness_files)
     
     # Create the output CSV path with the timestamp
-    output_csv_path = os.path.join(os.path.dirname(metrics_csv_path), f"performance_report_{metrics_csv_path.split("metrics_")[1]}")
+    output_csv_path = os.path.join(os.path.dirname(metrics_csv_path), f"performance_report_{metrics_csv_path.split('metrics_')[1]}")
 
     # Save to CSV
     final_report_df.to_csv(output_csv_path, index=False)
